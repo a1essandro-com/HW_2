4fb1209223cff579d53f37ddcd9baab7201b773e
Alex <aleksanderbabi4eff@yandex.ru>
2021-01-20 23:07:12 +0300

    add

--- HW2/src/app/main/main.component.ts
+++ HW2/src/app/main/main.component.ts
@@ -14,10 +14,12 @@ export class Item{
 export class MainComponent{
   item: Item = new Item();
   i: string[] = [this.item.city, this.item.country];
+  places: Array<{city:string,country:string}> = []
   constructor(private router: Router){ }
   goToItem(myItem: Item){
     var placeObject = { 'city': myItem.city, 'country': myItem.country };
     localStorage.setItem('place', JSON.stringify(placeObject));
-    this.i = [this.item.city, this.item.country]
+    //this.i = [this.item.city, this.item.country]
+    this.places.push.apply(myItem.city,myItem.country);
   }
 }
