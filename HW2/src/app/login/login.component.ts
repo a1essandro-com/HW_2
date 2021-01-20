1e53bd7b664a2de6d5c5c3c29ea262312c91a693
Alex <aleksanderbabi4eff@yandex.ru>
2021-01-20 22:11:23 +0300

    add

--- HW2/src/app/login/login.component.ts
+++ HW2/src/app/login/login.component.ts
@@ -15,11 +15,20 @@ export class Item{
 export class LoginComponent {
   item: Item = new Item();
   logina: string = "admin";
-  passworda: string = "admin";
+  passworda: string = "admin"
+  index: number=0 ;
   constructor(private router: Router){ }
   goToItem(myItem: Item){
     if(myItem.login == this.logina && myItem.password == this.passworda){
       location.replace("/admin");
+    }
+    else if (this.index == 1){
+      location.replace("/admin");
+    }
+    else{
+      localStorage.setItem('login', JSON.stringify(myItem.login));
+      localStorage.setItem('password', JSON.stringify(myItem.password));
+      this.index+=1;
     }
   }
 }
